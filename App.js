02c2012373aee57a43a7c3c957bf5210114f8976
Saloni Tanna <salonitanna6@gmail.com>
2024-04-01 15:33:06 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+      <Route path="/home" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<Signup/>}/>
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
